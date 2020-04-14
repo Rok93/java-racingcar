f5ed3d01c7d9bf9514ba8e8ebcff810e56be574e
김경록 <goodboy3421@gmail.com>
2020-04-14 23:22:02 +0900

    test: 차량을 생성하고, 승자를 얻는 기능 테스트 추가

---
+++ src/test/java/Controller/RacingGameSystemTest.java
@@ -0,0 +1,49 @@
+package Controller;
+
+import domain.Car;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RacingGameSystemTest {
+
+    private RacingGameSystem racingGameSystem;
+
+    @DisplayName("차량을 생성하는 기능을 테스트한다")
+    @Test
+    void test_generateCars() {
+        //given
+        racingGameSystem = new RacingGameSystem();
+        String carNames = "benz,zeep,kia";
+
+        //when
+        List<Car> cars = racingGameSystem.generateCars(carNames);
+
+        //then
+        assertThat(cars).hasSize(3);
+        assertThat(cars).contains(new Car("benz"), new Car("zeep"), new Car("kia"));
+    }
+
+    @DisplayName("승자를 얻는 기능을 테스트를 한다")
+    @Test
+    void test_getWinners() {
+        //given
+        Car benz = new Car("benz", 5);
+        Car kia = new Car("kia", 3);
+        Car zeep = new Car("zeep", 5);
+        Car honda = new Car("honda", 2);
+        List<Car> cars = Arrays.asList(benz, kia, zeep, honda);
+        racingGameSystem = new RacingGameSystem(cars);
+
+        //when
+        List<Car> winners = racingGameSystem.getWinners();
+
+        //then
+        assertThat(winners).hasSize(2);
+        assertThat(winners).contains(benz, zeep);
+    }
+}
