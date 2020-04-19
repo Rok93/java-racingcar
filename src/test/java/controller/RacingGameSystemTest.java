fa35390e983aa45303cf54c1a68ed166c906a0ef
김경록 <goodboy3421@gmail.com>
2020-04-20 02:21:35 +0900

    refactor: 변경된 기능에 따라 test 코드도 변경

--- src/test/java/controller/RacingGameSystemTest.java
+++ src/test/java/controller/RacingGameSystemTest.java
@@ -1,6 +1,7 @@
 package controller;
 
 import domain.Car;
+import domain.Cars;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,7 +9,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class RacingGameSystemTest {
 
@@ -18,15 +18,14 @@ class RacingGameSystemTest {
     @Test
     void test_generateCars() {
         //given
-        racingGameSystem = new RacingGameSystem();
-        List<String> carNames = Arrays.asList("benz", "zeep", "kia");
+        List<Car> carNames = Arrays.asList(new Car("benz"), new Car("zeep"), new Car("kia"));
 
         //when
-        List<Car> cars = racingGameSystem.generateCars(carNames);
+        Cars cars = new Cars(carNames);
 
         //then
-        assertThat(cars).hasSize(3);
-        assertThat(cars).contains(new Car("benz"), new Car("zeep"), new Car("kia"));
+        assertThat(cars.getCars()).hasSize(3);
+        assertThat(cars.getCars()).contains(new Car("benz"), new Car("zeep"), new Car("kia"));
     }
 
     @DisplayName("승자를 얻는 기능을 테스트를 한다")
@@ -37,7 +36,7 @@ class RacingGameSystemTest {
         Car kia = new Car("kia", 3);
         Car zeep = new Car("zeep", 5);
         Car honda = new Car("honda", 2);
-        List<Car> cars = Arrays.asList(benz, kia, zeep, honda);
+        Cars cars = new Cars(Arrays.asList(benz, kia, zeep, honda));
         racingGameSystem = new RacingGameSystem(cars);
 
         //when
