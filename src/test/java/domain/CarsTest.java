65ed1cb5acc157c87327f56a1891074a3470faaf
김경록 <goodboy3421@gmail.com>
2020-04-22 22:51:56 +0900

    test: Cars 모델의 깊은복사 기능을 테스트한다.

--- src/test/java/domain/CarsTest.java
+++ src/test/java/domain/CarsTest.java
@@ -2,7 +2,6 @@ package domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import utils.InputCarNameException;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,14 +15,17 @@ class CarsTest {
     @Test
     void test_generateCars() {
         //given
-        List<Car> carNames = Arrays.asList(new Car("benz"), new Car("zeep"), new Car("kia"));
+        Car benz = new Car("benz");
+        Car zeep = new Car("zeep");
+        Car kia = new Car("kia");
+        List<Car> carNames = Arrays.asList(benz, zeep, kia);
 
         //when
         Cars cars = new Cars(carNames);
 
         //then
         assertThat(cars.getCars()).hasSize(3);
-        assertThat(cars.getCars()).contains(new Car("benz"), new Car("zeep"), new Car("kia"));
+        assertThat(cars.getCars()).contains(benz, zeep, kia);
     }
 
 
@@ -35,7 +37,7 @@ class CarsTest {
 
         //when then
         assertThatThrownBy(() -> new Cars(carNames))
-                .isExactlyInstanceOf(InputCarNameException.class);
+                .isExactlyInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("차량이 이름이 중복되면 예외를 발생한다")
@@ -46,6 +48,41 @@ class CarsTest {
 
         //when then
         assertThatThrownBy(() -> new Cars(carNames))
-                .isExactlyInstanceOf(InputCarNameException.class);
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("Cars는 깊은복사를 한다")
+    @Test
+    void test_carsDeepCopy() {
+        //given
+        Car benz = new Car("benz");
+        Car kia = new Car("kia");
+
+        //when
+        Cars cars = new Cars(Arrays.asList(benz, kia));
+        Car copyBenz = cars.getCars().get(0);
+        Car copyKia = cars.getCars().get(1);
+
+        //then
+        assertThat(copyBenz.hashCode()).isNotEqualTo(benz.hashCode());
+        assertThat(copyKia.hashCode()).isNotEqualTo(kia.hashCode());
+    }
+
+    @DisplayName("승자를 얻는 기능을 테스트를 한다")
+    @Test
+    void test_getWinners() {
+        //given
+        Car benz = new Car("benz", 5);
+        Car kia = new Car("kia", 3);
+        Car zeep = new Car("zeep", 5);
+        Car honda = new Car("honda", 2);
+        Cars cars = new Cars(Arrays.asList(benz, kia, zeep, honda));
+
+        //when
+        List<Car> winners = cars.getWinners();
+
+        //then
+        assertThat(winners).hasSize(2);
+        assertThat(winners).contains(benz, zeep);
     }
 }
