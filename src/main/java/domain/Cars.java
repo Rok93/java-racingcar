86a89ecc4c2b5684ca22a4263d5d5692d7de06da
김경록 <goodboy3421@gmail.com>
2020-04-22 22:18:11 +0900

    refactor: Cars Model Class의 리팩토링
    깊은 복사를 위한 기능 추가(생성자)

--- src/main/java/domain/Cars.java
+++ src/main/java/domain/Cars.java
@@ -1,42 +1,71 @@
 package domain;
 
-import utils.InputCarNameException;
+import utils.RandomUtil;
 
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Cars {
     private static final int MIN_CAR_NUMBERS = 2;
     private static final String CAR_NUMBER_MIN_MESSAGE = "최소 2대의 차량이 있어야합니다.";
     private static final String CAR_NAME_DUPLICATED_MESSAGE = "차량의 이름은 중복되면 안됩니다.";
 
-    private List<Car> cars;
+    private List<Car> cars = new ArrayList<>();
 
     public Cars(List<Car> cars) {
-        isValidateCars(cars);
-        this.cars = cars;
-    }
-
-    private void isValidateCars(List<Car> carList) {
-        isNotDuplicatedCarName(carList);
-        isCarNumberSmallerThanTwo(carList);
-    }
-
-    private void isNotDuplicatedCarName(List<Car> carList) {
-        Set<Car> carSet = new HashSet<>(carList);
-        if (carList.size() != carSet.size()) {
-            throw new InputCarNameException(CAR_NAME_DUPLICATED_MESSAGE);
+        validateCars(cars);
+        for (Car car : cars) { //todo: ArrayList의 경우 addAll() 이라는 메서드로 깊은 복사가 가능하다고 하던데 생각한대로 잘 안됌
+            this.cars.add(new Car(car.getName(), car.getPosition()));
         }
     }
 
-    private void isCarNumberSmallerThanTwo(List<Car> carList) {
+    private void validateCars(List<Car> cars) {
+        validateName(cars);
+        validateCarNumber(cars);
+    }
+
+    private void validateName(List<Car> carList) { // Set <--> List의 Size 차이를 파악해서 해결할려고 했는데 생각대로 잘 안됌
+        int actualCarsSize = (int) carList.stream()
+                .map(Car::getName)
+                .distinct()
+                .count();
+
+        if (carList.size() != actualCarsSize) {
+            throw new IllegalArgumentException(CAR_NAME_DUPLICATED_MESSAGE);
+        }
+    }
+
+    private void validateCarNumber(List<Car> carList) {
         if (carList.size() < MIN_CAR_NUMBERS) {
-            throw new InputCarNameException(CAR_NUMBER_MIN_MESSAGE);
+            throw new IllegalArgumentException(CAR_NUMBER_MIN_MESSAGE);
         }
+    }
+
+    public List<Car> getWinners() {
+        int maxPosition = getMaxPosition();
+        return cars.stream()
+                .filter(car -> car.matchPosition(maxPosition))
+                .collect(Collectors.toList());
+    }
+
+    public int getMaxPosition() {
+        return this.cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .getAsInt();
     }
 
     public List<Car> getCars() {
         return cars;
+    }
+
+    public String play() {
+        StringBuilder sb = new StringBuilder();
+        for (Car car : cars) {
+            car.goOrStop(RandomUtil.generateRandomNumber());
+            sb.append(car.printCar());
+        }
+        return sb.toString();
     }
 }
