2b2d51cf4dfd23f5cab4d0b575c6d700f4c36e23
김경록 <goodboy3421@gmail.com>
2020-04-30 21:22:06 +0900

    refactor: 랜덤 숫자를 발생하는 기능을 랜덤 숫자에 따라 참, 거짓을 반환하는 기능으로 변경

--- src/main/java/utils/RandomUtil.java
+++ src/main/java/utils/RandomUtil.java
@@ -2,8 +2,13 @@ package utils;
 
 public class RandomUtil {
     private static final int MAX_RANDOM_NUMBER = 10;
+    private static final int STEP_FORWARD_STANDARD = 4;
 
-    public static int generateRandomNumber() {
-        return (int) (Math.random() * MAX_RANDOM_NUMBER);
+    public static boolean isGo() {
+        int randomNumber = (int) (Math.random() * MAX_RANDOM_NUMBER);
+        if (randomNumber >= STEP_FORWARD_STANDARD) {
+            return true;
+        }
+        return false;
     }
 }
