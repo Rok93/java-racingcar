8f5dda2028ad2d572e139138213408c3ca0f3713
김경록 <goodboy3421@gmail.com>
2020-05-01 22:54:47 +0900

    refactor: RandomUtil을 RacingCarMove 클래스로 변경, Move를 상속
    전략적으로 Car의 움직임을 제어할 수 있도록 구체적인 전략 설정

--- src/main/java/utils/RacingCarMove.java
+++ src/main/java/utils/RacingCarMove.java
@@ -1,10 +1,11 @@
 package utils;
 
-public class RandomUtil {
+public class RacingCarMove implements Move {
     private static final int MAX_RANDOM_NUMBER = 10;
     private static final int STEP_FORWARD_STANDARD = 4;
 
-    public static boolean isGo() {
+    @Override
+    public boolean isGo() {
         int randomNumber = (int) (Math.random() * MAX_RANDOM_NUMBER);
         if (randomNumber >= STEP_FORWARD_STANDARD) {
             return true;
