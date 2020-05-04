b8fc75f819965460adf570567ecca96282536fbb
김경록 <goodboy3421@gmail.com>
2020-05-04 15:50:01 +0900

    test: RacingGame의 playRacingGame 기능 테스트 코드 추가

--- src/test/java/domain/RacingGameTest.java
+++ src/test/java/domain/RacingGameTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -27,5 +28,34 @@ class RacingGameTest {
         assertThat(racingGame.getCars().getCars()).contains(benz, bmw);
         assertThat(racingGame.getTryNumber()).isNotEqualTo(tryNumber);
         assertThat(racingGame.getTryNumber().getTryNumber()).isEqualTo(number);
+    }
+
+    @DisplayName("레이싱게임을 플레이하는 기능을 테스트한다")
+    @Test
+    void test_playRacingGame() {
+        //given
+        Car benz = new Car("benz");
+        Car bmw = new Car("bmw");
+        Cars cars = new Cars(Arrays.asList(benz, bmw));
+        int carNumber = cars.getCars().size();
+        int number = 2;
+        TryNumber tryNumber = new TryNumber(number);
+
+        //when
+        RacingGame racingGame = new RacingGame(cars, tryNumber);
+        List<Cars> result = racingGame.playRacingGame(() -> true);
+        Cars resultCars1 = result.get(0);
+        Cars resultCars2 = result.get(1);
+
+        //then
+        assertThat(resultCars1.getCars().size()).isEqualTo(carNumber);
+        assertThat(resultCars1.getCars().get(0).getPosition()).isEqualTo(1);
+        assertThat(resultCars1.getCars().get(1).getPosition()).isEqualTo(1);
+        assertThat(resultCars1.getMaxPosition()).isEqualTo(1);
+
+        assertThat(resultCars2.getCars().size()).isEqualTo(carNumber);
+        assertThat(resultCars2.getCars().get(0).getPosition()).isEqualTo(2);
+        assertThat(resultCars2.getCars().get(1).getPosition()).isEqualTo(2);
+        assertThat(resultCars2.getMaxPosition()).isEqualTo(2);
     }
 }
